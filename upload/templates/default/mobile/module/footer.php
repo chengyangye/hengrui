f11940be125f5f7d94c96857770286741c23c943
yechengyang <yechengyang@yechengyangdeMac-mini.local>
2016-05-27 10:32:04 +0800

    重新提交

--- upload/templates/default/mobile/module/footer.php
+++
@@ -1,9 +0,0 @@
-{*<?php exit();?>*}
-<div id="footer">
-	<div class="info">
-		{include file="module/copyright.php"}
-		<a href="{$site.record_url}" target="_blank">{$site.record}</a>
-		{$site.statistical_code}
-	</div>
-</div>
-<!-- 新秀 -->
--- upload/templates/default/mobile/module/here.php
+++
@@ -1,5 +0,0 @@
-{*<?php exit();?>*}
-<div class="here">
-	<span>{if $cat_name}{$cat_name}{else}{$channel_title}{/if}</span>
-</div>
-<!-- 新秀 -->
